c144bc79af23251dffae0e57c370ba635000c007
AllenExtractor <dayarmustfa@gmail.com>
2026-05-21 18:53:00 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -9,10 +9,10 @@ BOT_TOKEN = environ.get("BOT_TOKEN", "")
 OWNER  = int(environ.get("OWNER", "8446475678"))
 CREDIT = environ.get("CREDIT", "💥 @CinderellaContactBot")
 
-TOTAL_USER  = os.environ.get('TOTAL_USERS', '8446475678').split(',')
+TOTAL_USER  = os.environ.get('TOTAL_USERS', '8446475678,8723278238,8480660521,7988815969,6660248311,8680968748,8745263057,8703802029,8446475678').split(',')
 TOTAL_USERS = [int(u) for u in TOTAL_USER if u.strip()]
 
-AUTH_USER  = os.environ.get('AUTH_USERS', '8446475678').split(',')
+AUTH_USER  = os.environ.get('AUTH_USERS', '8446475678,8723278238,8480660521,7988815969,6660248311,8680968748,8745263057,8703802029,8446475678').split(',')
 AUTH_USERS = [int(u) for u in AUTH_USER if u.strip()]
 if OWNER not in AUTH_USERS:
     AUTH_USERS.append(OWNER)
